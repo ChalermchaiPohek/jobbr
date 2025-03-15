daa6336e33b5621defb737f89498418b4d4bb586
Chalermchai Poh-ek <chalermchai.po@icloud.com>
2025-03-16 00:55:00 +0700

    Create new project.

---
+++ jobbr/jobbrApp.swift
@@ -0,0 +1,17 @@
+//
+//  jobbrApp.swift
+//  jobbr
+//
+//  Created by Chalermchai Poh-ek on 16/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct jobbrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
